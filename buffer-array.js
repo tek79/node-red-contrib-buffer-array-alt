9c34f11f52133466a1fb14dfbcbeebad800d6e96
tamio.honma <tamio.honma@apertomove.de>
2018-04-26 18:46:04 +0200

    Initial commit #WIP: Icon implementation & documentation

---
+++ buffer-array.js
@@ -0,0 +1,33 @@
+module.exports = function(RED) {
+
+  function BufferArray(config) {
+
+    RED.nodes.createNode(this,config);
+
+    const node = this;
+    const bufferLen = parseInt(config.bufferLen)||6;
+    let output = false;
+
+    node.on('input', function(msg) {
+
+      let bufferArray = output ||
+          (() => {
+            let ret = [];
+            for (let i = 0; i < bufferLen; i++) {
+              ret.push(0);
+            }
+            return ret;
+          })();
+
+      bufferArray.shift();
+      bufferArray.push(msg.payload);
+      output = bufferArray;
+
+      msg.payload = output;
+
+      node.send(msg);
+    });
+  }
+
+  RED.nodes.registerType('buffer-array', BufferArray);
+};
